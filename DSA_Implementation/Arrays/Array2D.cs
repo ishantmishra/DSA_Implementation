bc56d83732c4811a74eb008c0737f5a6707363bc
Ishant Mishra <ishant.mishra.1029@gmail.com>
2026-02-17 22:51:33 +0530

    first commit

---
+++ DSA_Implementation/Arrays/Array2D.cs
@@ -0,0 +1,26 @@
+﻿namespace DSA_Implementation.Arrays
+{
+    public class Array2D
+    {
+        public static void ArrayPrint()
+        {
+            int[,] array2d =
+            {
+                {1, 2, 3, 4 },
+                {5, 6, 7, 8 } 
+            };
+
+            int rows = array2d.GetLength(0);
+            int cols = array2d.GetLength(1);
+
+            for (int i = 0; i < rows ; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    Console.Write(array2d[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DSA_Implementation/Program.cs
@@ -0,0 +1,9 @@
+﻿using DSA_Implementation.Arrays;
+
+public class Program: Array2D
+{
+    public static void Main(string[] args)
+    {
+        ArrayPrint();
+    }
+}
